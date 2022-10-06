d3539f6b25b981f658a9170ab14129141a41f978
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-10-06 09:35:21 +0100

    Create equipo1.php

---
+++ actividadesRepaso/Actividad 3/equipo1.php
@@ -0,0 +1,81 @@
+<?php
+  $equipo = [
+    0 => [
+      "nombre" => "Álvaro Valles",
+      "posición" => "Portero",
+      "dorsal" => "13",
+      "altura" => "190",
+      "peso" => "78"
+    ],
+    1 => [
+      "nombre" => "Álex Suárez",
+      "posición" => "Lateral Derecho",
+      "dorsal" => "4",
+      "altura" => "179",
+      "peso" => "72"
+    ],
+    2 => [
+      "nombre" => "Sidnei",
+      "posición" => "Central",
+      "dorsal" => "18",
+      "altura" => "188",
+      "peso" => "81"
+    ],
+    3 => [
+      "nombre" => "Eric Curbelo",
+      "posición" => "Central",
+      "dorsal" => "6",
+      "altura" => "173",
+      "peso" => "69"
+    ],
+    4 => [
+      "nombre" => "Sergi Cardona",
+      "posición" => "Lateral Izquierdo",
+      "dorsal" => "3",
+      "altura" => "185",
+      "peso" => "76"
+    ],
+    5 => [
+      "nombre" => "Mfulu",
+      "posición" => "Centrocampista",
+      "dorsal" => "22",
+      "altura" => "182",
+      "peso" => "69"
+    ],
+    6 => [
+      "nombre" => "Loiodice",
+      "posición" => "Centrocampista",
+      "dorsal" => "12",
+      "altura" => "176",
+      "peso" => "60"
+    ],
+    7 => [
+      "nombre" => "Jonathan Viera",
+      "posición" => "Centrocampista",
+      "dorsal" => "21",
+      "altura" => "170",
+      "peso" => "64"
+    ],
+    8 => [
+      "nombre" => "Moleiro",
+      "posición" => "Delantero",
+      "dorsal" => "10",
+      "altura" => "169",
+      "peso" => "67"
+    ],
+    9 => [
+      "nombre" => "Marc Cardona",
+      "posición" => "Delantero",
+      "dorsal" => "19",
+      "altura" => "188",
+      "peso" => "81"
+    ],
+    10 => [
+      "nombre" => "Álvaro Jiménez",
+      "posición" => "Delantero",
+      "dorsal" => "15",
+      "altura" => "183",
+      "peso" => "70"
+    ]
+  ];
+?>
